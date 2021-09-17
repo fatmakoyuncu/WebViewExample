5c57b5fba8ed827bcea57fe6467c7caa7d31c321
fatmakoyuncu <fatmakyc16@gmail.com>
2021-09-17 15:15:16 +0300

    Initial Commit

---
+++ userAgentExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  userAgentExample
+//
+//  Created by simurg on 17.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
