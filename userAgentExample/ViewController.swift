78095eb7cfa133ec257686b1c80cde01ef9d2a2f
fatmakoyuncu <fatmakyc16@gmail.com>
2021-09-17 15:19:51 +0300

    initial commit

--- userAgentExample/ViewController.swift
+++ userAgentExample/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    let webView = WKWebView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        view.addSubview(webView)
+        
+        guard let url = URL(string: "https://ankageo.com") else {
+            return
+        }
+        
+        webView.load(URLRequest(url: url))
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        webView.frame = view.bounds
     }
 
 
