6151736ec667f5fb63074b7c6803d540de65d2d5
fatmakoyuncu <fatmakyc16@gmail.com>
2021-09-17 17:27:02 +0300

    get user agent

--- Ankageo/ViewController.swift
+++ Ankageo/ViewController.swift
@@ -10,7 +10,17 @@ import WebKit
 
 class ViewController: UIViewController {
     
-    let webView = WKWebView()
+    let webView: WKWebView = {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+        
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = prefs
+        
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        
+        return webView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +32,17 @@ class ViewController: UIViewController {
         }
         
         webView.load(URLRequest(url: url))
+        webView.customUserAgent = "iPad/Chrome/SomethingRandom"
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+5) {
+            self.webView.evaluateJavaScript("document.body.innerHTML") {
+                result, error in
+                guard let html = result as? String, error == nil else {
+                    return
+                }
+                
+            }
+        }
     }
     
     override func viewDidLayoutSubviews() {
